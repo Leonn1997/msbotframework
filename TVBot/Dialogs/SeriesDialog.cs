f6da5b183db5ae5cd2ab91a088b9a798aafb93fc
Leonn1997 <leon.gottschick@web.de>
2017-06-01 01:10:20 +0200

    new version

--- TVBot/Dialogs/SeriesDialog.cs
+++ TVBot/Dialogs/SeriesDialog.cs
@@ -1,76 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using System.Web;
-using HttpUtils;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Framework.Builder.Witai;
 using Microsoft.Bot.Framework.Builder.Witai.Dialogs;
 using Microsoft.Bot.Framework.Builder.Witai.Models;
-using Newtonsoft.Json;
-using TVBot.Models;
+
 
 namespace TVBot.Dialogs
 {
     [Serializable]
-    [WitModel("7JJ6P6X2DRA7IMAZMYZATPNTI334BGPO")]
+    [WitModel("PUT TOKEN HERE")]
     public class SeriesDialog : WitDialog<object>
     {
-        [WitAction("getRecommendations")]
-        public async Task GetRecommendations(IDialogContext context, WitResult result)
-        {
-            var series = result.Entities["search_query"]
-                .Select(a => a.Value)
-                .Select(a => a.ToString() )
-                .ToList();
+       // [WitAction("PUT FUNCTION NAME HERE")]
 
-            var r = await FindSimilarShows(series);
-
-            if (r != null && r.Count > 0)
-            {
-                //this.WitContext.RemoveIfExists("no_result");
-                //this.WitContext["results"] = r;
-                this.WitContext.RemoveIfExists("no_results");
-                this.WitContext.AddOrUpdate("results", r);
-            } else
-            {
-                this.WitContext.RemoveIfExists("results");
-                this.WitContext.AddOrUpdate("no_result", null);
-
-            }
-
-        }
-
-        public async Task<List<String>> FindSimilarShows(List<String> shows)
-        {
-            var showParm = "similar?q=" + ExtractShowParms(shows) + "&type=shows&info=1&k=155111-EPort-ZKC22N7Q";
-            string endPoint = @"https://tastedive.com/api/";
-
-
-            var client = new RestClient(endPoint);
-            client.Method = HttpVerb.GET;
-            var json = client.MakeRequest(showParm);
-
-            var root = JsonConvert.DeserializeObject<RootObject>(json);
-
-            return root.Similar.Results.Where(a => a != null).Select(r => r.Name).ToList();
-        }
-
-        private static String ExtractShowParms(List<string> shows)
-        {
-            const string seperator = "%2C";
-            const string space = "+";
-            var builder = new StringBuilder();
-            foreach (var value in shows)
-            {
-                builder.Append(value.Replace(" ", space));
-                builder.Append(seperator);
-            }
-
-            return builder.ToString();
-        }
     }
 }
---
+++ TVBot/Models/RecommendationService.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using HttpUtils;
+using TVBot.Models;
+using Newtonsoft.Json;
+using System.Linq;
+using System.Text;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class RecommendationService
+{
+	public RecommendationService()
+	{
+        
+    }
+    public async Task<String[]> FindSimilarShows(List<String> shows)
+    {
+        var showParm = "similar?q=" + ExtractShowParms(shows) + "&type=shows&info=1&k=155111-EPort-ZKC22N7Q";
+        string endPoint = @"https://tastedive.com/api/";
+
+
+        var client = new RestClient(endPoint);
+        client.Method = HttpVerb.GET;
+        var json = client.MakeRequest(showParm);
+
+        var root = JsonConvert.DeserializeObject<RootObject>(json);
+
+        return root.Similar.Results.Where(a => a != null).Select(r => r.Name).ToArray();
+    }
+
+    public async Task<String> PrittyResult(List<String> shows)
+    {
+        var l = await FindSimilarShows(shows);
+        var builder = new StringBuilder();
+        for(int i = 0; i < l.Count(); i++)
+        {
+            if(i == l.Count() -1 )
+            {
+                builder.Append(" and ");
+                builder.Append(l[i]);
+            } else if(i == l.Count() - 2)
+            {
+                builder.Append(l[i]);
+            } else {
+                builder.Append(l[i]);
+                builder.Append(", ");
+            }
+           
+        }
+
+        return builder.ToString();
+    }
+
+    private static String ExtractShowParms(List<string> shows)
+    {
+        const string seperator = "%2C";
+        const string space = "+";
+        var builder = new StringBuilder();
+        foreach (var value in shows)
+        {
+            builder.Append(value.Replace(" ", space));
+            builder.Append(seperator);
+        }
+
+        return builder.ToString();
+    }
+}
